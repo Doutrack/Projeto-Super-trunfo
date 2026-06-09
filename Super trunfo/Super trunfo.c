ba19a44407ce0848bec98beffbfb382bdc4083d6
Roberto Victor <Doutrack.dev@gmail.com>
2026-06-09 05:19:37 +0000

    v3

--- Super trunfo/Super trunfo.c
+++ Super trunfo/Super trunfo.c
@@ -6,10 +6,10 @@ int main() {
     char Estado1[50];
     char Codigo1[50];
     char cidade1[50];
-    int Populacao1;
+    unsigned long int Populacao1;
     float Area1;
     float PIB1;
-    unsigned long int pontosTuristicos1;
+    int pontosTuristicos1;
     float Densidade1;
     float Pibper1;
     float superPoder1;
@@ -18,10 +18,10 @@ int main() {
     char Estado2[50];
     char Codigo2[50];
     char cidade2[50];
-    int Populacao2;
+    unsigned long int Populacao2;
     float Area2;
     float PIB2;
-    unsigned long int pontosTuristicos2;
+    int pontosTuristicos2;
     float Densidade2;
     float Pibper2;
     float superPoder2;
@@ -53,8 +53,8 @@ int main() {
 
     superPoder1 = Populacao1 + Area1 + PIB1 + pontosTuristicos1 + Pibper1 + (Densidade1/Area1);
 
-    printf("Carta 1\n Estado: %s\n  Codigo: %s\n  Cidade: %s\n  População: %lu\n  Area: %.2f\n  PIB: %f\n  Pontos turisticos: %d\n  densidade: %f\n  Pibper: %f\n  Super Poder: %.2f", Estado1, Codigo1, cidade1, Populacao1, Area1, PIB1, pontosTuristicos1, Densidade1, Pibper1, superPoder1);
-
+    printf("Carta 1\n Estado: %s\n  Codigo: %s\n  Cidade: %s\n  População: %lu\n  Area: %.2f\n  PIB: %.2f\n  Pontos turisticos: %d\n  densidade: %.2f\n  Pibper: %.2f\n  Super Poder: %.2f\n", Estado1, Codigo1, cidade1, Populacao1, Area1, PIB1, pontosTuristicos1, Densidade1, Pibper1, superPoder1);
+    
     printf("Digite o estado\n");
     scanf("%s", Estado2);
 
@@ -80,7 +80,27 @@ int main() {
 
     Pibper2 = (PIB2 / Populacao2);
 
-    printf("Carta 2\n Estado: %s\n  Codigo: %s\n  Cidade: %s\n  População: %lu\n  Area: %.2f\n  PIB: %f\n  Pontos turisticos: %d\n  densidade: %f\n  Pibper: %f\n", Estado2, Codigo2, cidade2, Populacao2, Area2, PIB2, pontosTuristicos2, Densidade2, Pibper2);
+    superPoder2 = Populacao2 + Area2 + PIB2 + pontosTuristicos2 + Pibper2 + (Densidade2/Area2);
+
+    printf("Carta 2\n Estado: %s\n  Codigo: %s\n  Cidade: %s\n  População: %lu\n  Area: %.2f\n  PIB: %.2f\n  Pontos turisticos: %d\n  densidade: %.2f\n  Pibper: %.2f\n Super poder: %.2f\n", Estado2, Codigo2, cidade2, Populacao2, Area2, PIB2, pontosTuristicos2, Densidade2, Pibper2, superPoder2);
+    
+    unsigned long int População = Populacao1 > Populacao2;
+    float Area = Area1 > Area2;
+    float Pib = PIB1 > PIB2;
+    int Pontosturisticos = pontosTuristicos1 > pontosTuristicos2;
+    float densidade = Densidade1 < Densidade2;
+    float Pibper = Pibper1 > Pibper2;
+    float Superpoder = superPoder1 > superPoder2;
+
+    printf("O Grande resultado é:\n");
+
+    printf("População: %lu\n", População);
+    printf("Área: %.0f\n", Area);
+    printf("Pib: %.0f\n", Pib);
+    printf("Pontos turisticos: %d\n", Pontosturisticos);
+    printf("Densidade: %.0f\n", densidade);
+    printf("Pib Percap: %.0f\n", Pibper);
+    printf("Super poder: %.0f\n", Superpoder);
     
 
     return 0;
